e4acbc4e5d3504f0a65d253cdc108fde9de5dd6c
studytimee <studytimee@gmail.com>
2013-06-09 03:57:14 +0100

    Siging in with Twitter

--- lifestream/server/keys.js
+++ lifestream/server/keys.js
@@ -2,7 +2,7 @@
 exports.keyid  = 'AKIAJXT3QPYITZ3BQS4A'
 exports.secret = '6fTE76NVF+3gBpZqT+/ZW+G9/GnPFR6NUUwGnquS'
 
-exports.twitter  = { keyid:'YOUR_TWITTER_KEY_ID', secret:'YOUR_TWITTER_SECRET' }
+exports.twitter  = { keyid:'5Ce9pkIMCu6BifnrGylDg', secret:'Qdevmv4yWFBkxpwdF3c2EdTbgz1yJrK8R7T1WhXlhs' }
 exports.facebook = { keyid:'YOUR_FACEBOOK_KEY_ID', secret:'YOUR_FACEBOOK_SECRET' }
 exports.linkedin = { keyid:'YOUR_LINKEDIN_KEY_ID', secret:'YOUR_LINKEDIN_SECRET' }
 
